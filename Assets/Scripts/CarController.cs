2dcc15ded09025d087ac6d04990c95c544f8f9b7
Changbai Li <changbai.li@heliosinteractive.com>
2018-01-26 23:44:05 -0800

    Form prototype scene; reimport kinect example

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour {
+
+	public WheelJoint2D selfJoint;
+
+	JointMotor2D selfMotor;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
