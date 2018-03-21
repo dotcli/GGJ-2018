6f1dfe950fdc5aec1a0d696a5ebe7a3150b0ca22
Changbai Li <changbai.li@heliosinteractive.com>
2018-03-20 18:46:32 -0700

    added two levels with moving platform

---
+++ Assets/Scripts/HorizontalMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorizontalMovement : MonoBehaviour {
+
+	public float Amount = 1f;
+	public float Freq = 1f;
+	private float timer = 0f;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime;
+		Debug.Log((1 - Mathf.Cos(timer * Freq)) * Amount);
+		transform.localPosition = new Vector3((1 - Mathf.Cos(timer * Freq)) * Amount, 0f, 0f);
+	}
+}
---
+++ Assets/Scripts/VerticalMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerticalMovement : MonoBehaviour {
+
+	public float Amount = 1f;
+	public float Freq = 1f;
+	private float timer = 0f;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime;
+		Debug.Log((1 - Mathf.Cos(timer * Freq)) * Amount);
+		transform.localPosition = new Vector3(0f, (1 - Mathf.Cos(timer * Freq)) * Amount, 0f);
+	}
+}
